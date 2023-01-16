8e194a91d9b7eb91f0d0ad3b903c46b4b71ced70
Pierre <pierre.laur31@gmail.com>
2023-01-16 17:39:59 +0100

    Now plays the chords & melody

--- automatic-bach.py
+++ automatic-bach.py
@@ -1,53 +1,99 @@
-import argparse
-from musicpy import *
-import re
+import time
 from minizinc import Instance, Model, Solver
+from play_notes import Player
+import argparse
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
-def play_guitar() :
 
-    guitar = (C('CM7', 3, 1/4, 1/8)^2 |
-            C('G7sus', 2, 1/4, 1/8)^2 |
-            C('A7sus', 2, 1/4, 1/8)^2 |
-            C('Em7', 2, 1/4, 1/8)^2 | 
-            C('FM7', 2, 1/4, 1/8)^2 |
-            C('CM7', 3, 1/4, 1/8)@1 |
-            C('AbM7', 2, 1/4, 1/8)^2 |
-            C('G7sus', 2, 1/4, 1/8)^2) * 2
+def note_to_int(note):
+    if note == 'C':
+        return 0
+    if note in ['C#', 'Db']:
+        return 1
+    if note == 'D':
+        return 2
+    if note in ['D#', 'Eb']:
+        return 3
+    if note in ['E', 'Fb']:
+        return 4
+    if note in ['F', 'E#']:
+        return 5
+    if note in ['F#', 'Gb']:
+        return 6
+    if note == 'G':
+        return 7
+    if note in ['G#', 'Ab']:
+        return 8
+    if note == 'A':
+        return 9
+    if note in ['A#', 'Bb']:
+        return 10
+    if note in ['B', 'Cb']:
+        return 11
+    else:
+        print('Incorrect note')
+        return
 
-    play(guitar, bpm=100, instrument=25)
 
-def main() :
+def parse_chord(chord):
+    return list(map(note_to_int, chord[1:-1].split(', ')))
+
+
+def display_results(melody, result, a):
+    player = Player()
+    melody = list(map(note_to_int, melody))
+    if a:
+        for i in range(len(result)):
+            chords_notes, chord_names = result[i, "_output_item"].split('\n')
+            chords_notes = [parse_chord(chord)
+                            for chord in chords_notes.split('|')]
+            for chord_number, chord in enumerate(chords_notes):
+                player.play_chord(chord)
+                player.play_note(melody[chord_number]+12)
+                player.stop_playing()
+            print(f'Solution {i+1} :    {chord_names}')
+            time.sleep(1)
+    else:
+        chords_notes, chord_names = result["_output_item"].split('\n')
+        chords_notes = [parse_chord(chord)
+                        for chord in chords_notes.split('|')]
+
+        for chord_number, chord in enumerate(chords_notes):
+            player.play_chord(chord)
+            player.play_note(melody[i]+12)
+            player.stop_playing()
+        print(f'Solution  :    {chord_names}')
+
+    return
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("melody")
     parser.add_argument("--minimize_distance", action='store_true')
     parser.add_argument("--a", action='store_true')
 
     args = parser.parse_args()
-    melody = args.melody.strip().split() 
+    melody = args.melody.strip().split()
 
     model = Model("./automatic-bach.mzn")
-    if args.minimize_distance :
+    if args.minimize_distance:
         model.add_string("solve minimize sum(chord_distances)")
     solver = Solver.lookup("gecode")
     instance = Instance(solver, model)
     instance["melody"] = melody
+    instance["melody_length"] = len(melody)
 
-    def print_results(result, a) :
-        if a :
-            for i in range(len(result)) :
-                print(result[i,"_output_item"])
-        else : 
-            print(result)
+    if args.minimize_distance:
+        result = instance.solve(
+            intermediate_solutions=True if args.a else False)
 
-    if args.minimize_distance :
-        result = instance.solve(intermediate_solutions=True if args.a else False)
-        print_results(result,args.a)
-
-    else : 
+    else:
         result = instance.solve(all_solutions=True if args.a else False)
-        print_results(result,args.a)
 
-    play_guitar()
+    display_results(melody, result, args.a)
 
-if __name__=='__main__' :
+
+if __name__ == '__main__':
     main()
---
+++ play_notes.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame import mixer
+import time
+
+
+class Player:
+    def __init__(self) -> None:
+        self.notes = []
+        pygame.init()
+        pygame.mixer.set_num_channels(50)
+        for n in range(1, 26):
+            self.notes.append(mixer.Sound(f'notes/Piano1{n}.mp3'))
+
+    def play_chord(self, chord):
+        for note in chord:
+            self.notes[note].set_volume(0.5)
+            self.notes[note].play(0, 2000)
+
+    def play_note(self, note):
+        self.notes[note].set_volume(1.0)
+        self.notes[note].play(0, 2000)
+
+    def stop_playing(self):
+        time.sleep(1)
